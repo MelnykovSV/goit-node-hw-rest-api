8a642049fd5a3030c01d35f62c0b61f52be14832
MelnykovSV <svmelnykov@gmail.com>
2023-07-30 16:57:59 +0300

    1

--- validation/contactSchema.js
+++ validation/contactSchema.js
@@ -4,11 +4,11 @@ const contactSchema = Joi.object({
   name: Joi.string()
     .min(3)
     .max(20)
-    .pattern(/^[A-Za-z\s]+$/, "numbers")
+    .pattern(/^[A-Za-z\s]+$/, "Name")
     .required(),
   email: Joi.string().email().required(),
   phone: Joi.string()
-    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/, "numbers")
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/, "Phone")
     .required(),
 });
 

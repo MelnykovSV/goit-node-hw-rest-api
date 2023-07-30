7de66b267655dfb0252460875c8cb08863c80c53
MelnykovSV <svmelnykov@gmail.com>
2023-07-30 17:36:59 +0300

    1

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,9 +51,6 @@ const loginUser = async (req, res) => {
     throw HttpError(401, "Email or password invalid");
   }
 
-  if (!user.verify) {
-    throw HttpError(401, "Email not verified");
-  }
   const { _id, email, subscription, userName, password } = user;
 
   const isPasswordCorrect = await bcrypt.compare(loginPassword, password);
